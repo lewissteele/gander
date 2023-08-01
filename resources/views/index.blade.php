de258d6e35fefe6a3ad8b140f3e287e03e30bc49
Lewis Steele <lewissteele254@gmail.com>
2023-08-01 18:55:49 +0100

    Install daisyUI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Gander</title>
+        <title>{{ config('app.name') }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased">
+        <button class="btn">Button</button>
     </body>
 </html>
