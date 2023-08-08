311325cbd1f76104e83d3e503d491dd4b5e7c677
Lewis Steele <lewissteele254@gmail.com>
2023-08-08 18:54:20 +0100

    Add modal

--- resources/views/livewire/database-connection-form.blade.php
+++ resources/views/livewire/database-connection-form.blade.php
@@ -1,3 +1,8 @@
-<div>
-    {{-- The whole world belongs to you. --}}
-</div>
+<dialog class="modal">
+  <form method="dialog" class="modal-box">
+    <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+    <div class="modal-action">
+        <button class="btn">Close</button>
+    </div>
+  </form>
+</dialog>
