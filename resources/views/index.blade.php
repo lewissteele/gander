7d7648bf7a60a8d0272f2812e134dfea30c22e90
Lewis Steele <lewissteele254@gmail.com>
2023-08-08 18:53:52 +0100

    Add sidebar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html data-theme="dark">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,6 +7,14 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased">
-        <button class="btn">Button</button>
+        <div class="navbar bg-base-100">
+            <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+            <select class="select select-bordered w-full max-w-xs">
+                <option disabled selected>Who shot first?</option>
+                <option>Han Solo</option>
+                <option>Greedo</option>
+            </select>
+        </div>
+        <livewire:sidebar />
     </body>
 </html>
