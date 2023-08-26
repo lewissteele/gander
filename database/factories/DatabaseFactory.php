1c6876e0cd06380ade2931a72f5152cdbad158a1
Lewis Steele <lewissteele254@gmail.com>
2023-08-26 17:14:04 +0100

    Update factory

--- database/factories/DatabaseFactory.php
+++ database/factories/DatabaseFactory.php
@@ -9,15 +9,14 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class DatabaseFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'driver' => 'sqlite',
+            'hostname' => fake()->domainName(),
+            'username' => fake()->userName(),
+            'password' => fake()->password(),
+            'port' => fake()->randomNumber(),
         ];
     }
 }
