06affa9d04bf7761c03d470ed2ebab05c8896c41
Lewis Steele <lewissteele254@gmail.com>
2023-08-26 18:39:54 +0100

    Test it saves to the database

--- tests/Feature/Livewire/CreateDatabaseTest.php
+++ tests/Feature/Livewire/CreateDatabaseTest.php
@@ -4,16 +4,41 @@ namespace Tests\Feature\Livewire;
 
 use App\Livewire\CreateDatabase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
 use Livewire\Livewire;
 use Tests\TestCase;
 
 class CreateDatabaseTest extends TestCase
 {
-    /** @test */
-    public function renders_successfully()
+    use RefreshDatabase;
+
+    public function test_renders_successfully(): void
     {
+        Livewire::test(CreateDatabase::class)->assertStatus(Response::HTTP_OK);
+    }
+
+    public function test_saves_to_database(): void
+    {
+        $driver = 'pgsql';
+        $hostname = fake()->domainName();
+        $password = fake()->password();
+        $port = 5432;
+        $username = fake()->userName();
+
         Livewire::test(CreateDatabase::class)
-            ->assertStatus(200);
+            ->set('form.driver', $driver)
+            ->set('form.hostname', $hostname)
+            ->set('form.password', $password)
+            ->set('form.port', $port)
+            ->set('form.username', $username)
+            ->call('save');
+
+        $this->assertDatabaseHas('databases', [
+            'driver' => $driver,
+            'hostname' => $hostname,
+            'password' => $password,
+            'port' => $port,
+            'username' => $username,
+        ]);
     }
 }
