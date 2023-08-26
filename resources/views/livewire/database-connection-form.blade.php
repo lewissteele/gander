743ecf70967ea09c086e200d3c81f3c03861eaa3
Lewis Steele <lewissteele254@gmail.com>
2023-08-26 17:03:08 +0100

    Tidy up index

--- resources/views/livewire/database-connection-form.blade.php
+++
@@ -1,8 +0,0 @@
-<dialog class="modal">
-  <form method="dialog" class="modal-box">
-    <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-    <div class="modal-action">
-        <button class="btn">Close</button>
-    </div>
-  </form>
-</dialog>
