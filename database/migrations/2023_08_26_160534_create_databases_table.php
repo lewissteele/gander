113ba02e46f452fc99878c114f10da194a130d30
Lewis Steele <lewissteele254@gmail.com>
2023-08-26 17:10:28 +0100

    Add columns to database table

--- database/migrations/2023_08_26_160534_create_databases_table.php
+++ database/migrations/2023_08_26_160534_create_databases_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('databases', function (Blueprint $table) {
             $table->id();
+            $table->string('driver');
+            $table->text('hostname');
+            $table->text('username');
+            $table->text('password');
+            $table->integer('port')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('databases');
