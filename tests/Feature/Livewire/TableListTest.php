d8c5a94a494e14e4503184b4a6b9b40b76282c2b
Lewis Steele <lewissteele254@gmail.com>
2023-08-28 17:13:39 +0100

    Test table list renders ok

--- tests/Feature/Livewire/TableListTest.php
+++ tests/Feature/Livewire/TableListTest.php
@@ -3,15 +3,32 @@
 namespace Tests\Feature\Livewire;
 
 use App\Livewire\TableList;
+use App\Models\Database;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
 use Livewire\Livewire;
 use Tests\TestCase;
 
 class TableListTest extends TestCase
 {
-    /** @test */
-    public function renders_successfully()
+    use RefreshDatabase;
+
+    public function test_listing_tables_from_database(): void
     {
+        $expectedTableName = 'test_table';
+        $schemaBuilder = Database::factory()
+            ->create()
+            ->getUserConnection()
+            ->getSchemaBuilder();
+
+        $schemaBuilder->create(
+            $expectedTableName,
+            fn(Blueprint $table) => $table->boolean('test_column'),
+        );
+
         Livewire::test(TableList::class)
-            ->assertStatus(200);
+            ->assertStatus(Response::HTTP_OK)
+            ->assertSee($expectedTableName);
     }
 }
