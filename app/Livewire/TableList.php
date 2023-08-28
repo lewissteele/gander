7c054138b9f95b37ebdadd51d93b655ec449727a
Lewis Steele <lewissteele254@gmail.com>
2023-08-28 19:36:57 +0100

    Use table model

--- app/Livewire/TableList.php
+++ app/Livewire/TableList.php
@@ -4,29 +4,25 @@ namespace App\Livewire;
 
 use App\Models\Database;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class TableList extends Component
 {
     public string $active;
 
-    public Collection $tables;
+    public Database $database;
 
     public function mount(): void
     {
-        $database = Database::firstOrFail();
-        $tables = $database->getTablesNames();
-
-        $this->active = $tables->first();
-        $this->tables = $tables;
+        $this->database = Database::firstOrFail();
+        $this->active = $this->database->tables->first()->getTable();
     }
 
     public function render(): View
     {
         return view('livewire.table-list')->with([
             'active' => $this->active,
-            'tables' => $this->tables,
+            'tables' => $this->database->tables,
         ]);
     }
 }
