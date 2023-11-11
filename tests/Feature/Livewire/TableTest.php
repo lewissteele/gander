78e19ae430ad09921d0d8cef67c971e0b63d4fd3
Lewis Steele <lewissteele254@gmail.com>
2023-11-11 16:30:34 +0000

    Fix table test

--- tests/Feature/Livewire/TableTest.php
+++ tests/Feature/Livewire/TableTest.php
@@ -3,11 +3,14 @@
 namespace Tests\Feature\Livewire;
 
 use App\Livewire\Table;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 use Tests\TestCase;
 
 class TableTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function renders_successfully()
     {
