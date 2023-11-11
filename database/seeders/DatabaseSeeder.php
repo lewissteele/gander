d76715b72f2284f562d4c91847f9d1e25e02364f
Lewis Steele <lewissteele254@gmail.com>
2023-11-11 16:28:36 +0000

    Get connection using db facade

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,23 @@ namespace Database\Seeders;
 use App\Models\Database;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
         $database = Database::factory()->create();
-        $schemaBuilder = $database->getUserConnection()->getSchemaBuilder();
+        $connection = DB::connection($database->connection_name);
 
-        $schemaBuilder->create('test_table', function (Blueprint $table) {
-            $table->string('test_string_column');
-        });
+        $connection->getSchemaBuilder()->create(
+            'test_table',
+            function (Blueprint $table) {
+                $table->string('test_string_column');
+            },
+        );
 
-        $database->getUserConnection()->table('test_table')->insert([
+        $connection->table('test_table')->insert([
             ['test_string_column' => fake()->text()],
             ['test_string_column' => fake()->text()],
             ['test_string_column' => fake()->text()],
