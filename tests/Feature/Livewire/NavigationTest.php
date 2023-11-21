46a055576ce079cb07217664dc63a1ada29f45a1
Lewis Steele <lewissteele254@gmail.com>
2023-11-21 16:01:25 +0000

    Pint fix

--- tests/Feature/Livewire/NavigationTest.php
+++ tests/Feature/Livewire/NavigationTest.php
@@ -3,8 +3,6 @@
 namespace Tests\Feature\Livewire;
 
 use App\Livewire\Navigation;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
 use Tests\TestCase;
 
