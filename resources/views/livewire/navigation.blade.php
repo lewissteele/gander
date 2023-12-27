51bec4ee97689124bde568095237f70ed35b011e
Lewis Steele <lewissteele254@gmail.com>
2023-12-27 18:47:20 +0000

    Show host and database in dropdown

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,11 +1,9 @@
 <div class="navbar bg-base-100">
     <a class="btn btn-ghost text-xl">Gander</a>
     <select wire:model.live="database" class="select select-bordered w-full max-w-xs">
-        {{--<option disabled selected>Select database</option>--}}
-
         @foreach ($databases as $database)
             <option value="{{ $database->id }}">
-                {{ $database->host }}
+                {{ $database->database }} @ {{ $database->host }}
             </option>
         @endforeach
     </select>
